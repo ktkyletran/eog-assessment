bb09432ee5c6131f530b1f32ff10a42d7135c79c
ktkyletran <ktkyletran@gmail.com>
2021-07-10 11:04:08 -0500

    added live data to metric cards

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend } from 'recharts';
 import moment from 'moment';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
+import MetricCard from './MetricCard'
+// import { actions } from '../Features/Measurements/reducer';
+import Grid from '@material-ui/core/Grid'
 
 const useStyles = makeStyles({
   div: {
     textAlign: 'center',
     margin: '0 auto'
   },
+  grid: {
+    margin: '10px auto'
+  }
 });
 
 const Chart = () => {
   const classes = useStyles();
+  // const dispatch = useDispatch();
+  const subData = useSelector(state => state.subscription.subData);
   const activeMets = useSelector(state => state.metrics.activeMetrics);
   const measurementData = useSelector(state => state.measurements.measurements);
 
@@ -29,14 +37,73 @@ const Chart = () => {
   let xAxisFormatter = heartbeat => {
     return moment.unix(heartbeat).format('h:mmA');
   };
+
+  // useEffect(() => {
+  //   dispatch(actions.timeConvert(chartData))
+  // })
   
   return (
     <>
+      <Grid fluid="true" container row="true" spacing={2} className={classes.grid} alignItems="center">
+        {activeMets.map((metric, idx) => {
+          if (metric.metricName === 'injValveOpen') {
+            let valveArr = subData.find(element => element.metric === 'injValveOpen')
+            return (
+              <Grid item key={idx}>
+                <MetricCard key={idx} metric={metric.metricName} value={valveArr.value} unit={`${valveArr.unit}`} />
+              </Grid>
+            )
+          }
+          else if (metric.metricName === 'oilTemp') {
+            let valveArr = subData.find(element => element.metric === 'oilTemp')
+            return (
+              <Grid item key={idx}>
+                <MetricCard key={idx} metric={metric.metricName} value={valveArr.value} unit={`°${valveArr.unit}`} />
+              </Grid>
+            )
+          }
+          else if (metric.metricName === 'flareTemp') {
+            let valveArr = subData.find(element => element.metric === 'flareTemp')
+            return (
+              <Grid item key={idx}>
+                <MetricCard key={idx} metric={metric.metricName} value={valveArr.value} unit={`°${valveArr.unit}`} />
+              </Grid>
+            )
+          }
+          else if (metric.metricName === 'waterTemp') {
+            let valveArr = subData.find(element => element.metric === 'waterTemp')
+            return (
+              <Grid item key={idx}>
+                <MetricCard key={idx} metric={metric.metricName} value={valveArr.value} unit={`°${valveArr.unit}`} />
+              </Grid>
+            )
+          }
+          else if (metric.metricName === 'tubingPressure') {
+            let valveArr = subData.find(element => element.metric === 'tubingPressure')
+            return (
+              <Grid item key={idx}>
+                <MetricCard key={idx} metric={metric.metricName} value={valveArr.value} unit={` ${valveArr.unit}`} />
+              </Grid>
+            )
+          }
+          else if (metric.metricName === 'casingPressure') {
+            let valveArr = subData.find(element => element.metric === 'casingPressure')
+            return (
+              <Grid item key={idx}>
+                <MetricCard key={idx} metric={metric.metricName} value={valveArr.value} unit={` ${valveArr.unit}`} />
+              </Grid>
+            )
+          }
+          else {
+            return null;
+          }
+        })}
+      </Grid>
       <ResponsiveContainer height="50%" width="50%" className={classes.div}>
       <LineChart data={chartData}>
           <Legend align="right" verticalAlign="top"/>
           <CartesianGrid strokeDasharray="4 4" />
-          <XAxis type='category' tickFormatter={xAxisFormatter} dataKey="at" allowDuplicatedCategory={false} minTickGap={25}/>
+          <XAxis type='number' tickFormatter={xAxisFormatter} dataKey="at" allowDuplicatedCategory={false} minTickGap={10} domain={['dataMin', 'dataMax']} />
           <YAxis />
           <Tooltip />
           <Legend />
@@ -50,7 +117,7 @@ const Chart = () => {
                 type="linear"
                 dataKey="value"
                 stroke={metric.color}
-                activeDot={{ r: 4 }}
+                dot={false}
                 />
             )
           }))}
--- src/components/MetricCard.js
+++ src/components/MetricCard.js
@@ -3,11 +3,12 @@ import Card from '@material-ui/core/Card';
 
 const MetricCard = (props) => {
   const { metric, value, unit } = props
+
   return (
     <div>
       <Card key={metric} style={{width: '200px', padding: '0 10px', marginTop: '10px'}}>
         <h3>{metric}:</h3>
-        <h1>{value} {unit}</h1>
+        <h1>{value}{unit}</h1>
       </Card>
     </div>
   )
