2ce765ad0dd495572dadecaa2d777e99d1c8f295
Vanity <ktkyletran@gmail.com>
2021-07-11 18:04:04 -0500

    added colors for each metric data line and updated time format on x axis

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,25 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend } from 'recharts';
 import moment from 'moment';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
-import MetricCard from './MetricCard'
-// import { actions } from '../Features/Measurements/reducer';
-import Grid from '@material-ui/core/Grid'
+import MetricCard from './MetricCard';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles({
   div: {
     textAlign: 'center',
-    margin: '0 auto'
+    margin: '1% auto'
   },
-  grid: {
-    margin: '10px auto'
-  }
 });
 
 const Chart = () => {
   const classes = useStyles();
-  // const dispatch = useDispatch();
   const subData = useSelector(state => state.subscription.subData);
   const activeMets = useSelector(state => state.metrics.activeMetrics);
   const measurementData = useSelector(state => state.measurements.measurements);
@@ -31,83 +26,134 @@ const Chart = () => {
       };
     };
   };
-
+  
   const chartData = measurementData.filter(dataFilter)
-
+  
   let xAxisFormatter = heartbeat => {
-    return moment.unix(heartbeat).format('h:mmA');
+    const start = moment.unix(heartbeat);
+    const remainder = 60 - (start.minute() % 60);
+
+    return moment.unix(heartbeat).add(remainder, "minutes").format('h:mmA');
+    // return moment.unix(heartbeat).format('h:mmA');
   };
 
-  // useEffect(() => {
-  //   dispatch(actions.timeConvert(chartData))
-  // })
+  const assignColor = metric => {
+    if (metric === 'injValveOpen') {
+      return "red"
+    }
+    else if (metric === 'oilTemp') {
+      return "blue"
+    }
+    else if (metric === 'waterTemp') {
+      return "orange"
+    }
+    else if (metric === 'flareTemp') {
+      return "green"
+    }
+    else if (metric === 'casingPressure') {
+      return "hotpink"
+    }
+    else if (metric === 'tubingPressure') {
+      return "purple"
+    }
+  }
   
   return (
     <>
-      <Grid fluid="true" container row="true" spacing={2} className={classes.grid} alignItems="center">
+      <Grid fluid="true" container row="true" spacing={2} alignItems="center" justifyContent="center" className={classes.div}>
         {activeMets.map((metric, idx) => {
           if (metric.metricName === 'injValveOpen') {
-            let valveArr = subData.find(element => element.metric === 'injValveOpen')
-            return (
-              <Grid item key={idx}>
-                <MetricCard key={idx} metric={metric.metricName} value={valveArr.value} unit={`${valveArr.unit}`} />
-              </Grid>
-            )
+            try {
+              let metricArr = subData.find(element => element.metric === 'injValveOpen')
+              return (
+                <Grid item key={idx}>
+                  <MetricCard key={idx} metric={metric.metricName} value={metricArr.value} unit={`${metricArr.unit}`} />
+                </Grid>
+              )
+            }
+            catch {
+              return null;
+            }
           }
           else if (metric.metricName === 'oilTemp') {
-            let valveArr = subData.find(element => element.metric === 'oilTemp')
-            return (
-              <Grid item key={idx}>
-                <MetricCard key={idx} metric={metric.metricName} value={valveArr.value} unit={`°${valveArr.unit}`} />
-              </Grid>
-            )
+            let metricArr = subData.find(element => element.metric === 'oilTemp')
+            try {
+              return (
+                <Grid item key={idx}>
+                  <MetricCard key={idx} metric={metric.metricName} value={metricArr.value} unit={`°${metricArr.unit}`} />
+                </Grid>
+              )
+            }
+            catch {
+              return null;
+            }
           }
           else if (metric.metricName === 'flareTemp') {
-            let valveArr = subData.find(element => element.metric === 'flareTemp')
-            return (
-              <Grid item key={idx}>
-                <MetricCard key={idx} metric={metric.metricName} value={valveArr.value} unit={`°${valveArr.unit}`} />
-              </Grid>
-            )
+            let metricArr = subData.find(element => element.metric === 'flareTemp')
+            try {
+              return (
+                <Grid item key={idx}>
+                  <MetricCard key={idx} metric={metric.metricName} value={metricArr.value} unit={`°${metricArr.unit}`} />
+                </Grid>
+              )
+            }
+            catch {
+              return null;
+            }
           }
           else if (metric.metricName === 'waterTemp') {
-            let valveArr = subData.find(element => element.metric === 'waterTemp')
-            return (
-              <Grid item key={idx}>
-                <MetricCard key={idx} metric={metric.metricName} value={valveArr.value} unit={`°${valveArr.unit}`} />
-              </Grid>
-            )
+            let metricArr = subData.find(element => element.metric === 'waterTemp')
+            try {
+              return (
+                <Grid item key={idx}>
+                  <MetricCard key={idx} metric={metric.metricName} value={metricArr.value} unit={`°${metricArr.unit}`} />
+                </Grid>
+              )
+            }
+            catch {
+              return null;
+            }
           }
           else if (metric.metricName === 'tubingPressure') {
-            let valveArr = subData.find(element => element.metric === 'tubingPressure')
-            return (
-              <Grid item key={idx}>
-                <MetricCard key={idx} metric={metric.metricName} value={valveArr.value} unit={` ${valveArr.unit}`} />
-              </Grid>
-            )
+            let metricArr = subData.find(element => element.metric === 'tubingPressure')
+            try {
+              return (
+                <Grid item key={idx}>
+                  <MetricCard key={idx} metric={metric.metricName} value={metricArr.value} unit={` ${metricArr.unit}`} />
+                </Grid>
+              )
+            }
+            catch {
+              return null;
+            }
           }
           else if (metric.metricName === 'casingPressure') {
-            let valveArr = subData.find(element => element.metric === 'casingPressure')
-            return (
-              <Grid item key={idx}>
-                <MetricCard key={idx} metric={metric.metricName} value={valveArr.value} unit={` ${valveArr.unit}`} />
-              </Grid>
-            )
+            let metricArr = subData.find(element => element.metric === 'casingPressure')
+            try {
+              return (
+                <Grid item key={idx}>
+                  <MetricCard key={idx} metric={metric.metricName} value={metricArr.value} unit={` ${metricArr.unit}`} />
+                </Grid>
+              )
+            }
+            catch {
+              return null;
+            }
           }
           else {
             return null;
           }
         })}
       </Grid>
-      <ResponsiveContainer height="50%" width="50%" className={classes.div}>
+      <ResponsiveContainer height="65%" width="65%" className={classes.div}>
       <LineChart data={chartData}>
           <Legend align="right" verticalAlign="top"/>
-          <CartesianGrid strokeDasharray="4 4" />
-          <XAxis type='number' tickFormatter={xAxisFormatter} dataKey="at" allowDuplicatedCategory={false} minTickGap={10} domain={['dataMin', 'dataMax']} />
+          <CartesianGrid strokeDasharray="6 6" />
+          <XAxis type='number' tickFormatter={xAxisFormatter} dataKey="at" allowDuplicatedCategory={true} domain={['dataMin', 'dataMax']} />
           <YAxis />
           <Tooltip />
-          <Legend />
-          {chartData.map(((metric) => {   
+          {/* <Legend /> */}
+          {chartData.map(((metric) => {  
             return (
               <Line
                 name={metric.metric}
@@ -116,7 +162,7 @@ const Chart = () => {
                 unit={" " + metric.measurements[0].unit}
                 type="linear"
                 dataKey="value"
-                stroke={metric.color}
+                stroke={assignColor(metric.metric)}
                 dot={false}
                 />
             )
