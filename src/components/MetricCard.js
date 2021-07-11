8dc23b1525832c90af219da7c69fb0c081b1b824
Vanity <ktkyletran@gmail.com>
2021-07-11 18:04:55 -0500

    changed inline styling to custom style

--- src/components/MetricCard.js
+++ src/components/MetricCard.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Card from '@material-ui/core/Card';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  card: {
+    width: '200px',
+    padding: '0 10px'
+  },
+});
 
 const MetricCard = (props) => {
   const { metric, value, unit } = props
+  const classes = useStyles();
 
   return (
     <div>
-      <Card key={metric} style={{width: '200px', padding: '0 10px', marginTop: '10px'}}>
+      <Card key={metric} className={classes.card}>
         <h3>{metric}:</h3>
         <h1>{value}{unit}</h1>
       </Card>
